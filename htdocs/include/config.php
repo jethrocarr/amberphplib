73ee008f0e7f251a551648cd6017b418a63a0e18
Jethro Carr <jethro.carr@jethrocarr.com>
2009-10-26 00:15:02 +0000

    * Adding update configuration values

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -4,6 +4,24 @@
 
 	Your real configuration file is in config-settings.php
 */
+
+
+/*
+	Define Application Name & Versions
+*/
+
+// define the application details
+$GLOBALS["config"]["app_name"]			= "Amberphplib";
+$GLOBALS["config"]["app_version"]		= "1";
+
+// define the schema version required
+$GLOBALS["config"]["schema_version"]		= "20091026";
+
+
+
+/*
+	Inherit User Configuration & Database Connectivity
+*/
 include("config-settings.php");
 include("database.php");
 
