b55ee7f1e27bcca07d29b5b5f9a9ffabf95e64f0
Jethro Carr <jethro.carr@jethrocarr.com>
2013-08-17 15:49:49 +1200

    Merged new SQL structure

--- sql/amberphplib_template.sql
+++ sql/amberphplib_template.sql
@@ -1,290 +1,371 @@
--- phpMyAdmin SQL Dump
--- version 3.2.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.13, for Linux (i686)
 --
--- Host: localhost
--- Generation Time: Oct 26, 2009 at 01:04 PM
--- Server version: 5.0.77
--- PHP Version: 5.2.11
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: localhost    Database: oss-amberphplib
+-- ------------------------------------------------------
+-- Server version	5.6.13
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `amberphplib`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `config`
 --
 
-CREATE TABLE IF NOT EXISTS `config` (
+DROP TABLE IF EXISTS `config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `config` (
   `name` varchar(255) NOT NULL,
   `value` text NOT NULL,
-  PRIMARY KEY  (`name`)
+  PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `config`
 --
 
-INSERT INTO `config` (`name`, `value`) VALUES
-('APP_MYSQL_DUMP', '/usr/bin/mysqldump'),
-('APP_PDFLATEX', '/usr/bin/pdflatex'),
-('BLACKLIST_ENABLE', 'enabled'),
-('BLACKLIST_LIMIT', '10'),
-('DATA_STORAGE_LOCATION', 'use_database'),
-('DATA_STORAGE_METHOD', 'database'),
-('DATEFORMAT', 'yyyy-mm-dd'),
-('LANGUAGE_DEFAULT', 'en_us'),
-('LANGUAGE_LOAD', 'preload'),
-('PATH_TMPDIR', '/tmp'),
-('PHONE_HOME', 'enabled'),
-('PHONE_HOME_TIMER', ''),
-('SUBSCRIPTION_ID', ''),
-('SUBSCRIPTION_SUPPORT', 'opensource'),
-('TIMEZONE_DEFAULT', 'SYSTEM'),
-('UPLOAD_MAXBYTES', '5242880');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `file_uploads`
---
-
-CREATE TABLE IF NOT EXISTS `file_uploads` (
-  `id` int(11) NOT NULL auto_increment,
-  `customid` int(11) NOT NULL default '0',
-  `type` varchar(20) NOT NULL,
-  `timestamp` bigint(20) unsigned NOT NULL default '0',
-  `file_name` varchar(255) NOT NULL,
-  `file_size` varchar(255) NOT NULL,
-  `file_location` char(2) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-
--- --------------------------------------------------------
+LOCK TABLES `config` WRITE;
+/*!40000 ALTER TABLE `config` DISABLE KEYS */;
+INSERT INTO `config` VALUES ('APP_MYSQL_DUMP','/usr/bin/mysqldump'),('APP_PDFLATEX','/usr/bin/pdflatex'),('AUTH_METHOD','sql'),('BLACKLIST_ENABLE','disabled'),('BLACKLIST_LIMIT','10'),('DATA_STORAGE_LOCATION','use_database'),('DATA_STORAGE_METHOD','database'),('DATEFORMAT','yyyy-mm-dd'),('LANGUAGE_DEFAULT','en_us'),('LANGUAGE_LOAD','preload'),('PATH_TMPDIR','/tmp'),('PHONE_HOME','disabled'),('PHONE_HOME_TIMER',''),('SUBSCRIPTION_ID',''),('SUBSCRIPTION_SUPPORT','opensource'),('TIMEZONE_DEFAULT','SYSTEM'),('UPLOAD_MAXBYTES','5242880');
+/*!40000 ALTER TABLE `config` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `file_upload_data`
 --
 
-CREATE TABLE IF NOT EXISTS `file_upload_data` (
-  `id` int(11) NOT NULL auto_increment,
-  `fileid` int(11) NOT NULL default '0',
+DROP TABLE IF EXISTS `file_upload_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `file_upload_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fileid` int(11) NOT NULL DEFAULT '0',
   `data` blob NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table for use as database-backed file storage system' AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for use as database-backed file storage system';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `file_upload_data`
+--
+
+LOCK TABLES `file_upload_data` WRITE;
+/*!40000 ALTER TABLE `file_upload_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `file_upload_data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `file_uploads`
+--
+
+DROP TABLE IF EXISTS `file_uploads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `file_uploads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customid` int(11) NOT NULL DEFAULT '0',
+  `type` varchar(20) NOT NULL,
+  `timestamp` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `file_name` varchar(255) NOT NULL,
+  `file_size` varchar(255) NOT NULL,
+  `file_location` char(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `file_uploads`
+--
+
+LOCK TABLES `file_uploads` WRITE;
+/*!40000 ALTER TABLE `file_uploads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `file_uploads` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `journal`
 --
 
-CREATE TABLE IF NOT EXISTS `journal` (
-  `id` int(11) NOT NULL auto_increment,
-  `locked` tinyint(1) NOT NULL default '0',
+DROP TABLE IF EXISTS `journal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `journal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `locked` tinyint(1) NOT NULL DEFAULT '0',
   `journalname` varchar(50) NOT NULL,
   `type` varchar(20) NOT NULL,
-  `userid` int(11) NOT NULL default '0',
-  `customid` int(11) NOT NULL default '0',
-  `timestamp` bigint(20) unsigned NOT NULL default '0',
+  `userid` int(11) NOT NULL DEFAULT '0',
+  `customid` int(11) NOT NULL DEFAULT '0',
+  `timestamp` bigint(20) unsigned NOT NULL DEFAULT '0',
   `content` text NOT NULL,
   `title` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `journalname` (`journalname`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `journal`
+--
+
+LOCK TABLES `journal` WRITE;
+/*!40000 ALTER TABLE `journal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `journal` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `language`
 --
 
-CREATE TABLE IF NOT EXISTS `language` (
-  `id` int(11) NOT NULL auto_increment,
+DROP TABLE IF EXISTS `language`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `language` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `language` varchar(20) NOT NULL,
   `label` varchar(255) NOT NULL,
   `translation` text NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `language` (`language`),
   KEY `label` (`label`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=292 ;
+) ENGINE=InnoDB AUTO_INCREMENT=292 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `language`
+--
+
+LOCK TABLES `language` WRITE;
+/*!40000 ALTER TABLE `language` DISABLE KEYS */;
+/*!40000 ALTER TABLE `language` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `language_avaliable`
 --
 
-CREATE TABLE IF NOT EXISTS `language_avaliable` (
-  `id` int(11) NOT NULL auto_increment,
+DROP TABLE IF EXISTS `language_avaliable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `language_avaliable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(5) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `language_avaliable`
 --
 
-INSERT INTO `language_avaliable` (`id`, `name`) VALUES
-(1, 'en_us');
-
--- --------------------------------------------------------
+LOCK TABLES `language_avaliable` WRITE;
+/*!40000 ALTER TABLE `language_avaliable` DISABLE KEYS */;
+INSERT INTO `language_avaliable` VALUES (1,'en_us');
+/*!40000 ALTER TABLE `language_avaliable` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `menu`
 --
 
-CREATE TABLE IF NOT EXISTS `menu` (
-  `id` int(11) NOT NULL auto_increment,
-  `priority` int(11) NOT NULL default '0',
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `priority` int(11) NOT NULL DEFAULT '0',
   `parent` varchar(50) NOT NULL,
   `topic` varchar(50) NOT NULL,
   `link` varchar(50) NOT NULL,
-  `permid` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=170 ;
+  `permid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=170 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `menu`
+--
 
--- --------------------------------------------------------
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `permissions`
 --
 
-CREATE TABLE IF NOT EXISTS `permissions` (
-  `id` int(11) NOT NULL auto_increment,
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `value` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Stores all the possible permissions' AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Stores all the possible permissions';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `permissions`
 --
 
-INSERT INTO `permissions` (`id`, `value`, `description`) VALUES
-(1, 'disabled', 'Enabling the disabled permission will prevent the user from being able to login.'),
-(2, 'admin', 'Provides access to user and configuration management features (note: any user with admin can provide themselves with access to any other section of this program)');
-
--- --------------------------------------------------------
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (1,'disabled','Enabling the disabled permission will prevent the user from being able to login.'),(2,'admin','Provides access to user and configuration management features (note: any user with admin can provide themselves with access to any other section of this program)');
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `username` varchar(255) NOT NULL,
-  `realname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `password_salt` varchar(20) NOT NULL,
-  `contact_email` varchar(255) NOT NULL,
-  `time` bigint(20) NOT NULL default '0',
-  `ipaddress` varchar(15) NOT NULL,
-  PRIMARY KEY  (`id`),
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `realname` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `password_salt` varchar(20) NOT NULL DEFAULT '',
+  `contact_email` varchar(255) NOT NULL DEFAULT '',
+  `time` bigint(20) NOT NULL DEFAULT '0',
+  `ipaddress` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
   KEY `ipaddress` (`ipaddress`),
   KEY `time` (`time`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='User authentication system.' AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='User authentication system.';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `realname`, `password`, `password_salt`, `contact_email`, `time`, `ipaddress`) VALUES
-(1, 'setup', 'Setup Account', '14c2a5c3681b95582c3e01fc19f49853d9cdbb31', 'hctw8lbz3uhxl6sj8ixr', 'support@amberdms.com', 0, '');
-
--- --------------------------------------------------------
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'setup','Setup Account','14c2a5c3681b95582c3e01fc19f49853d9cdbb31','hctw8lbz3uhxl6sj8ixr','support@amberdms.com',0,'');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users_blacklist`
 --
 
-CREATE TABLE IF NOT EXISTS `users_blacklist` (
-  `id` int(11) NOT NULL auto_increment,
+DROP TABLE IF EXISTS `users_blacklist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_blacklist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `ipaddress` varchar(15) NOT NULL,
-  `failedcount` int(11) NOT NULL default '0',
-  `time` bigint(20) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Prevents automated login attacks.' AUTO_INCREMENT=1 ;
+  `failedcount` int(11) NOT NULL DEFAULT '0',
+  `time` bigint(20) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Prevents automated login attacks.';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users_blacklist`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `users_blacklist` WRITE;
+/*!40000 ALTER TABLE `users_blacklist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_blacklist` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users_options`
 --
 
-CREATE TABLE IF NOT EXISTS `users_options` (
-  `id` int(11) NOT NULL auto_increment,
-  `userid` int(11) NOT NULL default '0',
+DROP TABLE IF EXISTS `users_options`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_options` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL DEFAULT '0',
   `name` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=181 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=181 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users_options`
 --
 
--- --------------------------------------------------------
+LOCK TABLES `users_options` WRITE;
+/*!40000 ALTER TABLE `users_options` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_options` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users_permissions`
 --
 
-CREATE TABLE IF NOT EXISTS `users_permissions` (
-  `id` int(11) NOT NULL auto_increment,
-  `userid` int(11) NOT NULL default '0',
-  `permid` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Stores user permissions.' AUTO_INCREMENT=2 ;
+DROP TABLE IF EXISTS `users_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL DEFAULT '0',
+  `permid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Stores user permissions.';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users_permissions`
 --
 
-INSERT INTO `users_permissions` (`id`, `userid`, `permid`) VALUES (1, 1, 2);
-
--- --------------------------------------------------------
+LOCK TABLES `users_permissions` WRITE;
+/*!40000 ALTER TABLE `users_permissions` DISABLE KEYS */;
+INSERT INTO `users_permissions` VALUES (1,1,2);
+/*!40000 ALTER TABLE `users_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users_sessions`
 --
 
-CREATE TABLE IF NOT EXISTS `users_sessions` (
-  `id` int(11) NOT NULL auto_increment,
+DROP TABLE IF EXISTS `users_sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
   `authkey` varchar(40) NOT NULL,
-  `ipaddress` varchar(15) NOT NULL,
+  `ipv4` varchar(15) NOT NULL DEFAULT '',
+  `ipv6` varchar(255) NOT NULL DEFAULT '',
   `time` bigint(20) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users_sessions`
 --
 
+LOCK TABLES `users_sessions` WRITE;
+/*!40000 ALTER TABLE `users_sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Additional Changes
---
-
-INSERT INTO `config` (`name`, `value`) VALUES ('AUTH_METHOD', 'sql');
-
-
+-- Dump completed on 2013-08-17 15:48:13
